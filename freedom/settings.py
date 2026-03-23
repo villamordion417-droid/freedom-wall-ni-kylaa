3da9d1218f86ef48934d7e19a6ec62c0e1bab244
Dion <your-github-villamordion417@gmail.com>
2026-03-23 19:57:13 +0900

    Wiring up project and app URLs

--- freedom/settings.py
+++ freedom/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-default-key')
 
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 render_external_hostname = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if render_external_hostname:
     ALLOWED_HOSTS.append(render_external_hostname)

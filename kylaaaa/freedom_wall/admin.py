8cb1f68a49593ef7647e0a46230af7bba1c3195b
Dion <your-github-villamordion417@gmail.com>
2026-03-23 20:09:54 +0900

    Wiring up project and app URLs

--- kylaaaa/freedom_wall/admin.py
+++ kylaaaa/freedom_wall/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('emotion', 'created_at', 'content')
+    list_filter = ('emotion', 'created_at')
+    search_fields = ('content',)
